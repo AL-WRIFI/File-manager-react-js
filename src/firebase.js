f56621377fcd1217c127b4bda029fd8c824288c5
AL-WRIFI <alwrifi@soft@gmail.com>
2023-11-16 20:26:19 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBY6fQ7ST6ZSj_4LgDhHl6J2te18k_o4P0",
+
+  authDomain: "file-manager-reactjs.firebaseapp.com",
+
+  projectId: "file-manager-reactjs",
+
+  storageBucket: "file-manager-reactjs.appspot.com",
+
+  messagingSenderId: "29251293012",
+
+  appId: "1:29251293012:web:f6ac6bad65f2c756ec82c1"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
